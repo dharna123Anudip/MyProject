5ceb3028750383a08015273dd708e18199a272ce
dharna123Anudip <111556056+dharna123Anudip@users.noreply.github.com>
2022-08-19 10:55:38 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,24 @@
+package com.java;
+
+public class App {
+	public static void main(String args[])
+	{
+		
+		   int age=17;
+		   String result=age>=18?"Eligible to vote":"Not eligible to vote";
+		    System.out.println(result);
+		    }
+		
+	}
+
+
+//Conditional Operator(Ternary Operator)
+
+
+
+
+	
+	
+	
+	
+
